72a95a536964e3b64d177de59cf48459e06fb1e7
Swindle, Gregory Jay (Greg) <greg@swindle.net>
2019-02-07 17:56:13 -0600

    ci(code-climate): edit config
    - Install to the TRAVIS_BUILD_DIR
    - Run from the TRAVIS_BUILD_DIR
    - Try to exit 0 on failure.
    
    This is my last attempt to use cc-test-reporter before
    I uninstall it.
    
    #1

--- .github/ci/legal/fossa-init.sh
+++ .github/ci/legal/fossa-init.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 verify_fossa_cli() {
-  if [[ $(fossa -v) =~ "^fossa-cli version.*$" ]]
+  if test -n "$(fossa -v)"
   then
-    echo "FOSSA_INIT: OK: fossa-cli installed"
+    echo "[fossa-init]: OK: fossa-cli installed"
     return 0
   else
-    echo "FOSSA_INIT: ERR: fossa-cli not found."
+    echo "[fossa-init]: ERR: fossa-cli not found."
     return 1
   fi
 }
@@ -14,31 +14,31 @@ verify_fossa_cli() {
 verify_fossa_api_key() {
   if test -n "$FOSSA_API_KEY"
   then
-    echo "FOSSA_INIT: OK: Found FOSSA_API_KEY."
+    echo "[fossa-init]: OK: Found FOSSA_API_KEY."
     return 0
   else
-    echo "FOSSA_INIT: ERR: ENVVAR FOSSA_API_KEY not found."
+    echo "[fossa-init]: ERR: ENVVAR FOSSA_API_KEY not found."
     return 1
   fi
 }
 
 install_fossa_cli() {
-  echo "FOSSA_INIT: INSTALL: installing the fossa-cli"
+  echo "[fossa-init]: INSTALL: installing the fossa-cli"
   curl https://raw.githubusercontent.com/fossas/fossa-cli/master/install.sh | bash
 }
 
 run_fossa_analysis() {
-  echo "FOSSA_INIT: analyzing your dependencies' legal licenses"
+  echo "[fossa-init]: analyzing your dependencies' legal licenses"
   fossa analyze
 }
 
 run_fossa_test() {
-  echo "FOSSA_INIT: testing for legal use compliance risks"
+  echo "[fossa-init]: testing for legal use compliance risks"
   fossa test
 }
 
 main() {
-  if [ verify_fossa_cli ] && [ verify_fossa_api_key ]
+  if verify_fossa_cli && verify_fossa_api_key
   then
     fossa
   else
--- .github/ci/qa/code-climate-init.sh
+++ .github/ci/qa/code-climate-init.sh
@@ -1,28 +1,30 @@
-#!/bin/sh
-
-
+#!/bin/bash
 
 get_cc_test_reporter_url() {
-  CC_TEST_REPORTER_TYPE="$TRAVIS_OS_NAME"
   if [[ "$TRAVIS_OS_NAME" == "osx" ]]
   then
-    CC_TEST_REPORTER_TYPE="darwin"
+    echo "https://codeclimate.com/downloads/test-reporter/test-reporter-latest-darwin-amd64"
+  else
+    echo "https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64"
   fi
-  echo "https://codeclimate.com/downloads/test-reporter/test-reporter-latest-$CC_REPORTER_TYPE-amd64"
 }
 
 dl_cc_test_reporter() {
   CC_URL=$(get_cc_test_reporter_url)
-  curl -L "$CC_URL" > ./cc-test-reporter
+  curl -L "$CC_URL" > "$TRAVIS_BUILD_DIR/cc-test-reporter"
+  echo "[code-climate-init]: OK: Dowloaded script to $TRAVIS_BUILD_DIR/cc-test-reporter"
 }
 
 init_cc_test_reporter() {
-  chmod +x ./cc-test-reporter
+  chmod +x "$TRAVIS_BUILD_DIR/cc-test-reporter"
+  echo "[code-climate-init]: OK: Apply exec to $TRAVIS_BUILD_DIR/cc-test-reporter"
 }
 
 main() {
   dl_cc_test_reporter
   init_cc_test_reporter
+  echo "[code-climate-init]: OK: $TRAVIS_BUILD_DIR/cc-test-reporter is ready for use."
+  exit 0
 }
 
 main
